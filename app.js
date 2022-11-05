cbf86087437933f50a09dec4d01666c2d27d7407
Herschel TUIL <herschel.tuil@gmail.com>
2022-11-05 13:32:04 +0100

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 const port = process.env.APP_PORT ?? 5000;
+app.use(express.json()); 
+
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite user list");
@@ -15,7 +17,7 @@ const userHandler = require("./userHandler");
 
 app.get("/api/users", userHandler.getUsers);
 app.get("/api/users/:id", userHandler.getUsersById);
-
+app.post("/api/users", userHandler.postUser);
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
